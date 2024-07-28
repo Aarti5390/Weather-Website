cd92a86a08018c1583dcce8d55132b95dbfa3a38
Aarti5390 <168289400+Aarti5390@users.noreply.github.com>
2024-07-28 18:42:30 +0530

    Add files via upload

---
+++ Weather.js
@@ -0,0 +1,34 @@
+const url =
+	'https://api.openweathermap.org/data/2.5/weather';
+const apiKey =
+	'f00c38e0279b7bc85480c3fe775d518c';
+
+$(document).ready(function () {
+	weatherFn('Beed');
+});
+
+async function weatherFn(cName) {
+	const temp =
+		`${url}?q=${cName}&appid=${apiKey}&units=metric`;
+	try {
+		const res = await fetch(temp);
+		const data = await res.json();
+		if (res.ok) {
+			weatherShowFn(data);
+		} else {
+			alert('City not found. Please try again.');
+		}
+	} catch (error) {
+		console.error('Error fetching weather data:', error);
+	}
+}
+
+function weatherShowFn(data) {
+    $('#city-name').text(data.name);
+    $('#date').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    $('#temperature').html(`${data.main.temp.toFixed(1)}&deg;`);
+    $('#description').text(data.weather[0].description);
+    $('#wind-speed').html(`Wind Speed: ${data.wind.speed.toFixed(1)} m/s`);
+    $('#weather-icon').attr('src', `...`);
+    $('#weather-info').fadeIn();
+}
